a13a16c4685bb714bbf63bb1ee0d70f7089ffdab
ssdsrrewrr <ytmemefootball25@gmail.com>
2025-07-14 18:51:33 +0600

    Add .next/static/chunks/src_app_register_page_tsx_0d231561._.js 1752497490498

---
+++ .next/static/chunks/src_app_register_page_tsx_0d231561._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_06d51cd8._.js",
+  "static/chunks/src_ab8f9e95._.js"
+],
+    source: "dynamic"
+});
